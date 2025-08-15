fb3a6f9bc08297860cab33c8802f92d47c4ade6b
Aidan Wildhaber <awildhaber55@gmail.com>
2025-08-14 22:16:06 -0500

    finished book cover and started javascript

---
+++ script.js
@@ -0,0 +1,12 @@
+const form = document.getElementById('form');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    //Get book info
+    const author = document.getElementById('author_input');
+    const title = document.getElementById('book_title');
+    const pages = document.getElementById('total_pages');
+    const read = document.getElementById('readtf');
+    
+})
