86c1ab46bdd5ebdb9b16b3c035cdf632b2746ea8
Aidan Wildhaber <awildhaber55@gmail.com>
2025-08-20 02:46:20 -0500

    edited html, css, and deletes button when submitted. Notes also added

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
-const form = document.getElementById('form');
+const form = document.getElementById("form");
 
 form.addEventListener('submit', (e) => {
-    e.preventDefault();
 
-    //Get book info
-    const author = document.getElementById('author_input');
-    const title = document.getElementById('book_title');
-    const pages = document.getElementById('total_pages');
-    const read = document.getElementById('readtf');
+    e.preventDefault();
     
+    //Get form info
+    const author = document.getElementById('author_input').value;
+    const title = document.getElementById('book_title').value;
+    const pages = document.getElementById('total_pages').value;
+    const read = document.getElementById('readtf').value;
+
+    //Delete Submit button
+    document.getElementById('submit_btn').remove();
+
+    //Add open book button
+    const openBtn = document.createElement('button');
+    const btnText = document.getElementsByTagName('button').innerHTML = "Open Book";
+
+    form.append(openBtn)
 })
